895872b5324ac3271c890ed01377c7d68d61b8bf
Daniel Mateu <danielmateu86@gmail.com>
2023-03-22 13:55:20 +0100

    configureStore + slices

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount, resetValue } from './store/slices/counter/counterSlice'
 
 function App() {
   
-  const [count, setCount] = useState(0)
+
+  const {counter} = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>REDUX TOOLKIT</h1>
+        <h2>count is {counter}</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          Increment
         </button>
-        
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>
+          Increment by 2
+        </button>
+        <button onClick={() => dispatch(resetValue())}>
+          Reset
+        </button>
       </div>
-      
     </div>
   )
 }
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -1,9 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// const initialState = {
-//     value: 0,
-// }
-
 export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
@@ -11,26 +7,23 @@ export const counterSlice = createSlice({
     },
     reducers: {
         increment: (state) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.counter += 1
         },
-        // decrement: (state) => {
-        //     state.value -= 1
-        // },
-        // incrementByAmount: (state, action) => {
-        //     state.value += action.payload
-        // },
+        decrement: (state) => {
+            state.counter -= 1
+        },
+        resetValue: (state) => {
+            state.counter = 0
+        },
+        incrementByAmount: (state, action) => {
+            state.counter += action.payload
+        },
     },
 })
 
-// Action creators are generated for each case reducer function
 export const { 
     increment,
-    // decrement,
-    // incrementByAmount 
+    decrement,
+    resetValue,
+    incrementByAmount 
 } = counterSlice.actions
-
-// export default counterSlice.reducer
